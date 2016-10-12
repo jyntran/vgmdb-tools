8f9228ae0916032ba54d3a043d366d2c89e91f92
J. Tran <j.yn.tran@gmail.com>
2016-10-12 19:24:50 +0000

    Render tracklist and cover art in text format for easy foobar2000 tagging

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def index():
 
 @app.route('/album')
 def album():
-	return '<h1>VGMDB tools</h1><h2>Album</h2><form action="/album-search" method="POST"><label>URL: </label><input type="text" name="albuminput"><input type="submit" value="Submit"></input></form>'
+	return '<h1>VGMDB tools</h1><h2>Album</h2><form action="/album-tracks" method="POST"><label>URL: </label><input type="text" name="albuminput"><input type="submit" value="Submit"></input></form>'
 
 @app.route('/album-search', methods = [ 'POST' ])
 def album_search():
@@ -24,6 +24,48 @@ def album_search():
 				resp = r.text
 				return resp
 	return 'ERROR: album not found'
+
+@app.route('/album-tracks', methods = [ 'POST' ])
+def album_tracks():
+	album_input = request.form['albuminput']
+	tokens = album_input.split("/")
+	for i in tokens:
+		if i.isdigit():
+			album_id = i
+			r = requests.get('http://vgmdb.info/album/' + album_id + '?format=json')
+			if r.status_code==200:
+				data = json.loads(r.text) # dict
+			else:
+				resp = r.text
+				return resp
+	if data is not None:
+		# get cover art
+		cover = ''
+		if data['covers']:
+			for c in data['covers']:
+				if c['name']=='Front':
+					cover = c['full']
+
+		# get all languages
+		discs = data['discs']
+		track = discs[0]['tracks'][0]
+		languages = track['names']
+
+		tracks = {}
+		for l in languages:
+			tracks[l] = {'l':[], 't':''}
+			for d in data['discs']:
+				for t in d['tracks']:
+					tracks[l]['l'].append(t['names'][l])
+					tracks[l]['t'] = tracks[l]['t'] + t['names'][l] + '\n'
+		result = '<h1>Album Tracklist</h1><h2>' + data['name'] + '</h2>'
+		for l in languages:
+			result = result + '<h3>' + l + '</h3><textarea>' + tracks[l]['t'] + '</textarea>'
+		if cover:
+			result = result + '<h2>Cover Art</h2><img src="' + cover + '"/>'
+		return result
+	else:
+		return 'ERROR: album not found'
 
 if __name__ == '__main__':
 	application.run(host='0.0.0.0')
