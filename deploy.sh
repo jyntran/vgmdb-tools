905763899818875f4f444b53d2452ca96d59629c
J. Tran <j.yn.tran@gmail.com>
2018-05-31 00:41:19 +0000

    Old changes

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-uwsgi --http 0.0.0.0:5000 --module vgmdb --callab app
+#uwsgi --http 0.0.0.0:5000 --module vgmdb --callab app
+uwsgi --http-socket 0.0.0.0:5000 --module vgmdb --callab app --uid safeuser
 
